1cc3bdd2a46405c4a38a981abcf34ec777e2fa16
Peng <1998cpcp@gmail.com>
2022-04-30 07:42:53 +0000

    bug fix: deadlock in the receiver of output queue

--- e2e/trt/include/inference_client.h
+++ e2e/trt/include/inference_client.h
@@ -7,7 +7,8 @@
 #include <thread>
 #include <vector>
 #include <iostream>
-#include <atomic>
+#include <mutex>
+#include <condition_variable>
 
 #include "common.h"
 #include "ipc.h"
@@ -17,9 +18,11 @@ class InferenceClient {
  private:
   MessageQueueShm inputQueue;
   MessageQueueShm outputQueue;
-  std::atomic_size_t inputCount, outputCount;
-  std::atomic_bool finished;
+  size_t inputCount, outputCount;
+  bool finished;
   std::thread receiver;
+  std::mutex mutex;
+  std::condition_variable cond;
  public:
   InferenceClient(
       const std::string &inputQueueName,
@@ -34,25 +37,35 @@ class InferenceClient {
     if (msg) {
       memcpy(msg, data, size);
       inputQueue.finishWrite(msg);
+      mutex.lock();
       ++inputCount;
+      cond.notify_one();
+      mutex.unlock();
     } else {
       std::cerr << "Message too large" << std::endl;
     }
   }
 
   void sync() {
+    mutex.lock();
     finished = true;
+    cond.notify_one();
+    mutex.unlock();
     receiver.join();
     finished = false;
     receiver = std::thread([this] { discardResult(); });
   }
 
   void discardResult() {
-    while (!finished || inputCount < outputCount) {
+    while (true) {
+      std::unique_lock<std::mutex> lock(mutex);
+      while (!finished && inputCount <= outputCount) cond.wait(lock);
+      if (finished && inputCount == outputCount) break;
       size_t size;
       void *addr = outputQueue.startRead(&size);
       outputQueue.finishRead(addr);
       ++outputCount;
+      lock.unlock();
     }
 }
 };
